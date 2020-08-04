4379eca5f3137caa9ed2e452cbbc26af13c45409
Agatha North <rouborn@gmail.com>
2020-08-04 14:29:16 -0700

    app stuff

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Header from '../src/components/Header/Header'
+import PrivateRoute from '../src/routesPrivateRoute/PrivateRoute'
+import PublicRoute from '../src/routes/PublicRoute/PublicRoute'
+import RegistrationRoute from '../src/routes/RegistrationRoute/RegistrationRoute'
+import LoginRoute from '../src/routes/LoginRoute/LoginRoute'
+import DashboardRoute from '../src/routes/DashboardRoute/DashboardRoute'
+import LearningRoute from '../src/routes/LearningRoute/LearningRoute'
+import NotFoundRoute from '../src/routes/NotFoundRoute/NotFoundRoute'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+export default class App extends Component {
+  state = { hasError: false }
 
-      </header>
-    </div>
-  );
+  static getDerivedStateFromError(error) {
+    console.error(error)
+    return { hasError: true }
+  }
+
+  render() {
+    const { hasError } = this.state
+    return (
+      <div className='App'>
+        <Header />
+        <main>
+          {hasError && (
+            <p>There was an error! Oh no!</p>
+          )}
+          <Switch>
+            <PrivateRoute
+              exact
+              path={'/'}
+              component={DashboardRoute}
+            />
+            <PrivateRoute
+              path={'/learn'}
+              component={LearningRoute}
+            />
+            <PublicRoute
+              path={'/register'}
+              component={RegistrationRoute}
+            />
+            <PublicRoute
+              path={'/login'}
+              component={LoginRoute}
+            />
+            <Route
+              component={NotFoundRoute}
+            />
+          </Switch>
+        </main>
+      </div>
+    );
+  }
 }
-
-export default App;
