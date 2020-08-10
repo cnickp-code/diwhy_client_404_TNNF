f2b2d70d63816b0e75a745c04f66266358c84c7c
Agatha North <rouborn@gmail.com>
2020-08-10 08:20:51 -0700

    test

--- src/components/Browse/BrowseItem.js
+++ src/components/Browse/BrowseItem.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-export default class BrowseCategoryItem extends Component {
+export default class BrowseItem extends Component {
   render() {
     const { categories } = this.props;
     return (
