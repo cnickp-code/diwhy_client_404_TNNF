9c6336de0e1492c7410f518cff62313053462769
Magdalena Painter <paintermagdalena@gmail.com>
2020-08-12 13:25:51 -0400

    working on threads

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react'
 // import { Link } from 'react-router-dom'
-// import AppContext from '../../contexts/AppContext'
+import AppContext from '../../contexts/AppContext'
 import { Label } from '../Util/Util'
+import ThreadsApiService from '../../Services/threads-api-service';
 import './Feed.css'
 
 export default class Feed extends Component {
     constructor(props) {
         super(props);
-        this.state = { value: '' };
+        this.state = {
+            value: '',
+            threads: []
+        };
 
         this.handleChange = this.handleChange.bind(this);
     }
-    // static contextType = AppContext;
+
+    static contextType = AppContext
+
+    componentDidMount() {
+        this.context.clearError()
+        const threads = ThreadsApiService.getThreads()
+        this.setState({ threads })
+    }
 
     handleChange(e) {
         this.setState({
@@ -20,6 +31,31 @@ export default class Feed extends Component {
     }
 
     render() {
+        const { threads } = this.state;
+        console.log(threads)
+
+        const threadsList = threads.map(thread => {
+            return <div className="tl-header" key={thread.id}>
+                <div className="tl-pic-container">
+                    <img src="https://via.placeholder.com/100" className="tl-pic"></img>
+                </div>
+                <div className="tl-header-content">
+                    <div className="tl-name-container">
+                        <h2 className="hw-name">{this.context.user.user_name}</h2>
+                    </div>
+                    <div className="tl-title-container">
+                        <h3 className="tl-title"><i>{thread.title}</i></h3>
+                    </div>
+                    <div className='tl-options'>
+                        <div>Like</div>
+                        <div>Unanswered</div>
+                        <div>Add To Watch List</div>
+                    </div>
+                </div>
+
+            </div>
+        })
+
         return (
             <section className='dash-item'>
                 <h3 id='header'>Feed</h3>
@@ -35,24 +71,7 @@ export default class Feed extends Component {
                 </select>
                 <div className='tl-main-container'>
                     <div className="tl-item-container">
-                        <div className="tl-header">
-                            <div className="tl-pic-container">
-                                <img src="https://via.placeholder.com/100" className="tl-pic"></img>
-                            </div>
-                            <div className="tl-header-content">
-                                <div className="tl-name-container">
-                                    <h2 className="hw-name">user 1</h2>
-                                </div>
-                                <div className="tl-title-container">
-                                    <h3 className="tl-title"><i>title</i></h3>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='tl-options'>
-                            <div>Like</div>
-                            <div>Unanswered</div>
-                            <div>Add To Watch List</div>
-                        </div>
+                        {threadsList}
                     </div>
                 </div>
             </section>
