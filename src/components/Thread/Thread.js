e85b715d9678b8a4c0d6d6d7ac94116165b8758f
Agatha North <rouborn@gmail.com>
2020-08-13 09:38:26 -0700

    thread added

---
+++ src/components/Thread/Thread.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import './Thread.css'
+import AppContext from '../../contexts/AppContext'
+import ThreadApiService from '../../Services/want-api-service';
+// import { Link } from 'react-router-dom'
+// import Util from '../Util/Util'
+
+export default class Thread extends Component {
+    state = {
+        threads: []
+    }
+    static contextType = AppContext;
+
+    componentDidMount() {
+        const threads = ThreadApiService.getThread();
+        console.log(threads);
+
+        this.setState({
+            threads
+        })
+    }
+    render() {
+        const { threads } = this.state;
+
+        const threadList = threads.map(thread => {
+            return (
+                <div className='note sticky' key={thread.id}>
+                    <div className='tack'></div>
+                    <div className='note-content'>
+                        {thread.content}
+                    </div>
+                </div>
+            )
+        })
+        return (
+            <div id='want-wrapper'>
+                {threadList}
+            </div>
+        )
+    }
+}
+
