a57a1506b3ab3cc0ce8ef541555abfc9b3a16b7a
Agatha North <rouborn@gmail.com>
2020-08-19 11:33:17 -0700

    header fixed

--- src/components/HelpWantedItem/HelpWantedItem.js
+++ src/components/HelpWantedItem/HelpWantedItem.js
@@ -49,12 +49,7 @@ class HelpWantedItem extends React.Component {
     }
 
     render() {
-<<<<<<< HEAD
-        console.log(this.state.posting)
-        const applicantsList = this.state.applicants.map(applicant => {
-=======
         const applicantsList = this.context.applicants.map(applicant => {
->>>>>>> 1747814fa42ae70f59f562dd5b3e9746a159483e
             return (
                 <li className='applicant-list-item' key={applicant.id}>
                     <h2 className='application-header'>{applicant.user.user_name}</h2>
@@ -71,21 +66,12 @@ class HelpWantedItem extends React.Component {
                 {/* <div className="hw-pic-container">
                             <img src="https://via.placeholder.com/100" className="hw-pic"></img>
                         </div> */}
-<<<<<<< HEAD
-                <div className="hw-header-content">
-                    <h2 className="hw-name"> {this.state.posting.user_name}</h2>
-                    <h3 className="hw-title"><i>{this.state.posting.title}</i></h3>
-                </div>
-                <p>{this.state.posting.content}</p>
-                <div className="hw-body-footer">
-=======
                 <div className="hw-main-container">
                     <div className="hw-header-content">
                         <h2 className="hw-name"> {this.context.user.user_name}</h2>
                         <h3 className="hw-title"><i>{this.state.posting.title}</i></h3>
                     </div>
                     <p className='hw-content'>{this.state.posting.content}</p>
->>>>>>> 1747814fa42ae70f59f562dd5b3e9746a159483e
                     <p>Topic: {this.getCategoryName(this.state.posting.category)}</p>
                     <div className="hw-body-buttons">
                         {/* <button className="hw-btn">Apply</button> */}
