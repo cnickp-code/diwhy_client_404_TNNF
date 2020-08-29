9b55d85b0269e1532ce9d89eb322c7eca2367d3b
Victor Jarvis <vctrjrvs@gmail.com>
2020-08-29 00:28:02 -0400

    attempted tests, pre-deploy

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import AppContext, { AppProvider } from './contexts/AppContext';
+import App from './App/';
+import { AppProvider } from '../../contexts/AppContext';
 
 it('renders', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<BrowserRouter><AppProvider.Consumer><App /></AppProvider.Consumer></BrowserRouter>, div)
-  ReactDOM.unmountComponentAtNode(div)
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <BrowserRouter>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </BrowserRouter>, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
--- src/components/Dashboard/Dashboard.test.js
+++ src/components/Dashboard/Dashboard.test.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import Dashboard from './Dashboard';
+import Dashboard from '../Dashboard/Dashboard';
 import { AppProvider } from '../../contexts/AppContext';
 
 it('renders the Dashboard', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<BrowserRouter><AppProvider.Consumer><Dashboard /></AppProvider.Consumer></BrowserRouter>, div);
+  ReactDOM.render(
+    <BrowserRouter>
+      <AppProvider>
+        <Dashboard />
+      </AppProvider>
+    </BrowserRouter>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
